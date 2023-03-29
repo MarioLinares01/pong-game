2eb967c4d13ba5a06c59c7eb9ace393b7040247b
Mario Linares <mariolinares@csu.fullerton.edu>
2023-03-28 22:15:40 -0700

    Add player score and change player socre position

--- ponggame/scene.py
+++ ponggame/scene.py
@@ -66,7 +66,7 @@ class GameScene(Scene):
     def __init__(self, screen, background_color):
         super().__init__(screen, background_color)
         self._frame_rate = 60
-        self._ai_score = "0"
+        self._ai_score = 0
         self._player_score = 0
     
     def draw(self):
@@ -75,11 +75,11 @@ class GameScene(Scene):
         (width, height) = self._screen.get_size()
         midline = pygame.draw.line(self._screen, (0, 255, 0), [0, height / 2], [600, height / 2], width=3)
 
-        # add AI numberscore
-        ai_score_font = pygame.font.Font("ponggame/assets/fonts/square_sans_serif_7.ttf", 70)
-        ai_rendered_score = ai_score_font.render(self._ai_score, True, (0, 250, 0))
-        ai_position = ai_rendered_score.get_rect(center=(570, 270))
-        self._screen.blit(ai_rendered_score, ai_position)
-        print(ai_position)
-        # add player numberscore 
-        
+        score_font = pygame.font.Font("ponggame/assets/fonts/square_sans_serif_7.ttf", 70)
+        ai_rendered_score = score_font.render(str(self._ai_score), True, (0, 250, 0))
+        ai_score_position = ai_rendered_score.get_rect(center=(570, 272))
+        self._screen.blit(ai_rendered_score, ai_score_position)
+
+        player_rendered_score = score_font.render(str(self._player_score), True, (0, 250, 0))
+        player_score_position = player_rendered_score.get_rect(center=(570, 328))
+        self._screen.blit(player_rendered_score, player_score_position)
